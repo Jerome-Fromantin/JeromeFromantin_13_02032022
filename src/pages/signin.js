23d69fbd648cc40c2b18f9c8d2d8958a6758de64
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-03-02 18:34:12 +0100

    Début de mise en place selon code original

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import '../css/main.css'
 //import Xxx from '../composants/Xxx'
 
+/* Vérifier si la classe "main" est utilisée ailleurs... */
+
 function Signin() {
-    return (
-        <div>Rien pour l'instant dans la page "sign-in".
-        </div>
-    )
+    return (<div>
+        <main className="main bg-dark">
+            <section className="sign-in-content">
+                <i className="fa fa-user-circle sign-in-icon"></i>
+                <h1>Sign In</h1>
+                <form>
+                    <div className="input-wrapper">
+                        <label for="username">Username</label
+                        ><input type="text" id="username" />
+                    </div>
+                    <div className="input-wrapper">
+                        <label for="password">Password</label
+                        ><input type="password" id="password" />
+                    </div>
+                    <div className="input-remember">
+                        <input type="checkbox" id="remember-me" /><label for="remember-me"
+                        >Remember me</label>
+                    </div>
+                    {/*<!-- PLACEHOLDER DUE TO STATIC SITE -->
+                    <a href="./user.html" className="sign-in-button">Sign In</a>
+                    <!-- SHOULD BE THE BUTTON BELOW -->
+                    <!-- <button className="sign-in-button">Sign In</button> -->
+                    <!--  -->*/}
+                </form>
+            </section>
+        </main>
+    </div>)
 }
 
 export default Signin
