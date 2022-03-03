7c4f0ce67c28c9bd1ab9789f516df6bd2f291a01
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-03-03 16:56:21 +0100

    Amélioration du header et du footer

--- src/composants/Footer.js
+++ src/composants/Footer.js
@@ -1,17 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 function Footer() {
     return (<div>
         <footer className="footer">
             <p className="footer-text">Copyright 2020 Argent Bank</p>
         </footer>
-        <div className="footer-text">
-            <p>Lien provisoire</p>
-            <p>
-                <Link to="/user" className="link_apropos">User</Link>
-            </p>
-        </div>
     </div>)
 }
 
--- src/composants/Header.js
+++ src/composants/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import logo from '../img/argentBankLogo.png'
 
 function Header() {
+    const { id } = useParams()
     return (<div>
         <nav className="main-nav">
             <Link to="/" className="main-nav-link main-nav-logo">
@@ -13,12 +14,25 @@ function Header() {
                 />
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
-            <div>
-                <Link to="/sign-in" className="main-nav-link main-nav-item">
-                    <i className="fa fa-user-circle"></i>
-                    &nbsp;Sign In
-                </Link>
-            </div>
+            {!id ? (
+                <div>
+                    <Link to="/sign-in" className="main-nav-link main-nav-item">
+                        <i className="fa fa-user-circle"></i>
+                        &nbsp;Sign In
+                    </Link>
+                </div>
+            ) : (
+                <div>
+                    <Link to="/user/:id" className="main-nav-link main-nav-item">
+                        <i class="fa fa-user-circle"></i>
+                        &nbsp;Tony
+                    </Link>
+                    <Link to="/" className="main-nav-link main-nav-item">
+                        <i class="fa fa-sign-out"></i>
+                        &nbsp;Sign Out
+                    </Link>
+                </div>
+            )}
         </nav>
     </div>)
 }
