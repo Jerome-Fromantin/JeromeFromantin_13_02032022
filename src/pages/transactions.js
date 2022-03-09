2dd3752b8b3b727a303db504019dd89542ddfe9b
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-03-09 09:40:22 +0100

    Mise en place de la page Transactions

--- src/pages/transactions.js
+++ src/pages/transactions.js
@@ -5,28 +5,11 @@ import Testchecking from '../composants/testchecking'
 import Testsavings from '../composants/testsavings'
 import Testcredit from '../composants/testcredit'
 import TransTitle from '../composants/TransTitle'
+import DataLine from '../composants/DataLine'
 
 function Transactions() {
     const { checking, savings, creditCard } = useParams()
 
-    /*if (checking) {
-        return (<div>
-            <Testchecking/>
-            <div>Reste du contenu "checking".</div>
-        </div>)
-    }
-    else if (savings) {
-        return (<div>
-            <Testsavings/>
-            <div>Reste du contenu "savings".</div>
-        </div>)
-    }
-    else if (creditCard) {
-        return (<div>
-            <Testcredit/>
-            <div>Reste du contenu "credit card".</div>
-        </div>)
-    }*/
     return (
         <HelmetProvider>
             <Helmet>
@@ -37,7 +20,25 @@ function Transactions() {
                 <TransTitle title="Argent Bank Checking (x8349)" amount="$2,082.79"
                 amountDescr="Available Balance"/>
                 <main id="transpage" className="main bg-dark">
-                    <div>Reste du contenu "checking".</div>
+                    <div className="dataHead">
+                        <div id="arrow" className="dataLineItem"></div>
+                        <div id="date" className="dataLineItem">DATE</div>
+                        <div id="descr" className="dataLineItem">DESCRIPTION</div>
+                        <div id="amount" className="dataLineItem">AMOUNT</div>
+                        <div id="balance" className="dataLineItem">BALANCE</div>
+                    </div>
+                    <DataLine date="June 20th, 2020" descr="Golden Sun Bakery" amount="$5.00" balance="$2082.79"
+                    transType="Electronic" category="Food"/>
+                    <DataLine date="June 20th, 2020" descr="Golden Sun Bakery" amount="$10.00" balance="$2087.79"
+                    transType="Electronic" category="Food"/>
+                    <DataLine date="June 20th, 2020" descr="Golden Sun Bakery" amount="$20.00" balance="$2097.79"
+                    transType="Electronic" category="Food"/>
+                    <DataLine date="June 20th, 2020" descr="Golden Sun Bakery" amount="$30.00" balance="$2117.79"
+                    transType="Electronic" category="Food"/>
+                    <DataLine date="June 20th, 2020" descr="Golden Sun Bakery" amount="$40.00" balance="$2147.79"
+                    transType="Electronic" category="Food"/>
+                    <DataLine date="June 20th, 2020" descr="Golden Sun Bakery" amount="$50.00" balance="$2187.79"
+                    transType="Electronic" category="Food"/>
                 </main>
             </div>) : (
             savings ? (<div>
