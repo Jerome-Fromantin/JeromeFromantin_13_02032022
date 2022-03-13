6404d1f707f351fb68ef87280fa50a14945bcf98
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-03-13 18:34:16 +0100

    Utilisation du store de Redux améliorée

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
-import { getToken, getName, getPass } from '../redux/store'
+import { getToken1, getToken2, getName, getPass } from '../redux/store'
 
 function Signin() {
     const [username, setUsername] = useState("")
@@ -11,34 +11,41 @@ function Signin() {
 
     useEffect(() => {
         const testname = localStorage.getItem('username')
-        console.log("Le username est " + testname + ".")
+        console.log('Le username est "' + testname + '".')
     }, [username])
 
     useEffect(() => {
         const testpass = localStorage.getItem('password')
-        console.log("Le password est " + testpass + ".")
+        console.log('Le password est "' + testpass + '".')
     }, [password])
 
+    // Fonction utilisée au remplissage du 1er input.
     function handleName(event) {
         setUsername({username : event.target.value})
         let name = event.target.value
-        //console.log(name)
         localStorage.setItem('username', name)
-        //dispatch(getToken())
+        dispatch(getToken1())
         dispatch(getName())
     }
 
+    // Fonction utilisée au remplissage du 2ème input.
     function handlePass(event) {
         setPassword({password : event.target.value})
         let pass = event.target.value
-        //console.log(pass)
         localStorage.setItem('password', pass)
-        //dispatch(getToken())
+        dispatch(getToken2())
         dispatch(getPass())
     }
 
     //console.log(username)
+    // Ci-dessus affiche au départ : <empty string>
+    // Ajout d'un "f", affiche : Object { username: "f" }
+    // Retrait du "f", affiche : Object { username: "" }
+    
     //console.log(password)
+    // Ci-dessus affiche au départ : <empty string>
+    // Ajout d'un "f", affiche : Object { password: "f" }
+    // Retrait du "f", affiche : Object { password: "" }
 
     const dispatch = useDispatch()
 
@@ -47,20 +54,23 @@ function Signin() {
             "email": "tony@stark.com",
             "password": "password123"
         })
-        //console.log(response1)
         //console.log(response1.data.body.token)
-        localStorage.setItem('token_login1', response1.data.body.token)
-        localStorage.setItem('name_pass', response1.config.data)
+        // Ci-dessus affiche le token extrait de la réponse, renouvelé à chaque frappe quel que soit le champ.
         //console.log(response1.config.data)
+        // Ci-dessus affiche : {"email":"tony@stark.com","password":"password123"}
+        localStorage.setItem('token_login1', response1.data.body.token)
+        localStorage.setItem('name_pass1', response1.config.data)
 
         const response2 = await axios.post("http://localhost:3001/api/v1/user/login", {
             "email": "steve@rogers.com",
             "password": "password456"
         })
-        //console.log(response2)
         //console.log(response2.data.body.token)
-        localStorage.setItem('token_login2', response2.data.body.token)
+        // Ci-dessus affiche le token extrait de la réponse, renouvelé à chaque frappe quel que soit le champ.
         //console.log(response2.config.data)
+        // Ci-dessus affiche : {"email":"steve@rogers.com","password":"password456"}
+        localStorage.setItem('token_login2', response2.data.body.token)
+        localStorage.setItem('name_pass2', response2.config.data)
     }
     getDBData()
 
@@ -75,25 +85,16 @@ function Signin() {
                     <h1>Sign In</h1>
                     <form>
                         <div className="input-wrapper">
-                            <label htmlFor="username">Username</label
-                            >{/*<input type="text" id="username" value={username}
-                            onChange={(e) => setUsername(e.target.value)}/>*/}
+                            <label htmlFor="username">Username</label>
                             <input type="text" id="username" onChange={handleName}/>
-                            {/*<input type="text" id="username" onChange={() => {
-                                dispatch(getToken())
-                                dispatch(getName())
-                            }}/>*/}
-
+                            {/*<input type="text" id="username" value={username}
+                            onChange={(e) => setUsername(e.target.value)}/>*/}
                         </div>
                         <div className="input-wrapper">
-                            <label htmlFor="password">Password</label
-                            >{/*<input type="password" id="password" value={password}
-                            onChange={(e) => setPassword(e.target.value)}/>*/}
+                            <label htmlFor="password">Password</label>
                             <input type="password" id="password" onChange={handlePass}/>
-                            {/*<input type="text" id="username" onChange={() => {
-                                dispatch(getToken())
-                                dispatch(getPass())
-                            }}/>*/}
+                            {/*<input type="password" id="password" value={password}
+                            onChange={(e) => setPassword(e.target.value)}/>*/}
                         </div>
                         <div className="input-remember">
                             <input type="checkbox" id="remember-me" /><label htmlFor="remember-me"
