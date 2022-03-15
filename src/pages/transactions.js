9d250152a7edf1bb787aa34c94093e63b3b52144
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-03-15 18:48:17 +0100

    Finition de la page Transactions

--- src/pages/transactions.js
+++ src/pages/transactions.js
@@ -1,22 +1,16 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
-import Testchecking from '../composants/testchecking'
-import Testsavings from '../composants/testsavings'
-import Testcredit from '../composants/testcredit'
 import TransTitle from '../composants/TransTitle'
 import DataLine from '../composants/DataLine'
 
 function Transactions() {
-    const { checking, savings, creditCard } = useParams()
 
     return (
         <HelmetProvider>
             <Helmet>
                 <title>Argent Bank - Transactions Page</title>
             </Helmet>
-            {checking ? (<div>
-                <Testchecking/>
+            <div>
                 <TransTitle title="Argent Bank Checking (x8349)" amount="$2,082.79"
                 amountDescr="Available Balance"/>
                 <main id="transpage" className="main bg-dark">
@@ -40,24 +34,7 @@ function Transactions() {
                     <DataLine date="June 20th, 2020" descr="Golden Sun Bakery" amount="$50.00" balance="$2187.79"
                     transType="Electronic" category="Food"/>
                 </main>
-            </div>) : (
-            savings ? (<div>
-                <Testsavings/>
-                <TransTitle title="Argent Bank Savings (x6712)" amount="$10,928.42"
-                amountDescr="Available Balance"/>
-                <main id="transpage" className="main bg-dark">
-                    <div>Reste du contenu "savings".</div>
-                </main>
-            </div>) : (
-            creditCard ? (<div>
-                <Testcredit/>
-                <TransTitle title="Argent Bank Credit Card (x8349)" amount="$184.30"
-                amountDescr="Current Balance"/>
-                <main id="transpage" className="main bg-dark">
-                    <div>Reste du contenu "credit card".</div>
-                </main>
-            </div>) : ("test")))
-            }
+            </div>
         </HelmetProvider>
     )
 }
