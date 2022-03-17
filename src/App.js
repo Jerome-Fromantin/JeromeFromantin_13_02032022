229bb751ffff07c6b24b31af9d039f2827b96778
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-03-17 18:32:53 +0100

    Modifications mineures

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './pages/layout'
 import Home from './pages/home'
 import Signin from './pages/signin'
 import User from './pages/user'
 import Transactions from './pages/transactions'
+import './App.css'
 
 
 function App() {
@@ -15,8 +15,8 @@ function App() {
         <Route path="/" element={<Layout/>}>
           <Route index element={<Home/>}/>
           <Route path="login" element={<Signin/>}/>
-          <Route path="profile/:id" element={<User/>}/>
-          <Route path="profile/:id/:edit" element={<User/>}/>
+          <Route path="profile" element={<User/>}/>
+          <Route path="profile/edit" element={<User/>}/>
           <Route path="transactions/:id" element={<Transactions/>}/>
         </Route>
       </Routes>
